1cc606dbaaa5941445fe7a54619254a922ab902a
Victorvs1967 <victorsmirnov@me.com>
2023-07-01 11:14:48 +0300

    add font and images processor

---
+++ gulp/docs.js
@@ -0,0 +1,108 @@
+import gulp from "gulp";
+import fileInclude from 'gulp-file-include';
+import htmlclean from 'gulp-htmlclean';
+import * as Sass from 'sass';
+import gulpSass from 'gulp-sass';
+import sassGlob from 'gulp-sass-glob';
+import autoprefixer from 'gulp-autoprefixer';
+import csso from 'gulp-csso';
+import babel from 'gulp-babel';
+import webpack from 'webpack-stream';
+import imagemin from 'gulp-imagemin';
+import webp from 'gulp-webp';
+import webpHtml from 'gulp-webp-html';
+import webpCss from 'gulp-webp-css';
+import changed from 'gulp-changed';
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
+import { deleteAsync } from 'del';
+import server from 'gulp-server-livereload';
+
+import config from '../webpack.config.js';
+
+const { task, src, dest } = gulp;
+
+const sass = gulpSass(Sass);
+
+const fileInludeOptions = {
+  prefix: '@@',
+  basepath: '@file',
+};
+
+const sassOptions = {
+  outputStyle: 'compressed',
+};
+
+const serverOptions = {
+  livereload: true,
+  open: true,
+};
+
+const plumberNotify = title => ({
+  errorHandler: notify.onError({
+    title,
+    message: 'Error: <%= error.message %>',
+    sound: false,
+  })
+});
+
+task('html', () =>
+  src(['./src/html/**/*.html', '!./src/html/blocks/*.html'])
+    .pipe(changed('./docs/'))
+    .pipe(plumber(plumberNotify('HTML')))
+    .pipe(fileInclude(fileInludeOptions))
+    .pipe(webpHtml())
+    .pipe(htmlclean())
+    .pipe(dest('./docs/'))
+);
+
+task('sass', () =>
+  src('./src/sass/*.sass')
+    .pipe(changed('./docs/css/'))
+    .pipe(plumber(plumberNotify('SASS')))
+    .pipe(sassGlob())
+    .pipe(sass(sassOptions))
+    .pipe(autoprefixer())
+    .pipe(csso())
+    .pipe(webpCss())
+    .pipe(dest('./docs/css/'))
+);
+
+task('js', () =>
+  src('./src/js/*.js')
+    .pipe(changed('./docs/js/'))
+    .pipe(plumber(plumberNotify('JS')))
+    .pipe(babel())
+    .pipe(webpack(config))
+    .pipe(dest('./docs/js/'))
+);
+
+task('images', () =>
+  src('./src/img/**/*')
+    .pipe(changed('./docs/img/'))
+    .pipe(imagemin({ verbose: true }))
+    .pipe(dest('./docs/img/'))
+    .pipe(src('./src/img/**/*'))
+    .pipe(changed('./docs/img/'))
+    .pipe(webp())
+    .pipe(dest('./docs/img/'))
+);
+
+task('fonts', () =>
+  src('./src/fonts/**/*.ttf')
+    .pipe(changed('./docs/fonts/'))
+    .pipe(dest('./docs/fonts/'))
+);
+
+task('files', () =>
+  src('./src/files/**/*')
+    .pipe(changed('./docs/files/'))
+    .pipe(dest('./docs/files/'))
+);
+
+task('server', () =>
+  src('./docs')
+    .pipe(server(serverOptions))
+);
+
+task('clean', async () => await deleteAsync('./docs/', { force: true }));
--- gulpfile.js
+++ gulpfile.js
@@ -1,110 +1,17 @@
-import gulp from "gulp";
-import fileInclude from 'gulp-file-include';
-import * as Sass from 'sass';
-import gulpSass from 'gulp-sass';
-import sassGlob from 'gulp-sass-glob';
-// import groupMediaQuieries from 'gulp-group-css-media-queries';
-import babel from 'gulp-babel';
-import webpack from 'webpack-stream';
-import imagemin from 'gulp-imagemin';
-import changed from 'gulp-changed';
-import plumber from 'gulp-plumber';
-import notify from 'gulp-notify';
-import { deleteAsync } from 'del';
-import server from 'gulp-server-livereload';
+import gulp from 'gulp';
+import './gulp/dev.js';
+import './gulp/docs.js';
 
-import config from './webpack.config.js';
-
-const { task, series, parallel, src, dest, watch } = gulp;
-
-const sass = gulpSass(Sass);
-
-const fileInludeOptions = {
-  prefix: '@@',
-  basepath: '@file',
-};
-
-const sassOptions = {
-
-};
-
-const serverOptions = {
-  livereload: true,
-  open: true,
-};
-
-const plumberNotify = title => ({
-  errorHandler: notify.onError({
-    title,
-    message: 'Error: <%= error.message %>',
-    sound: false,
-  })
-});
-
-task('html', () =>
-  src(['./src/html/**/*.html', '!./src/html/blocks/*.html'])
-    .pipe(changed('./dist/'))
-    .pipe(plumber(plumberNotify('HTML')))
-    .pipe(fileInclude(fileInludeOptions))
-    .pipe(dest('./dist/'))
-);
-
-task('sass', () =>
-  src('./src/sass/*.sass', { sourcemaps: true })
-    .pipe(changed('./dist/css/'))
-    .pipe(plumber(plumberNotify('SASS')))
-    .pipe(sassGlob())
-    .pipe(sass())
-    // .pipe(groupMediaQuieries()) // break source mapping!!!
-    .pipe(dest('./dist/css/', { sourcemaps: true }))
-);
-
-task('js', () =>
-  src('./src/js/*.js', { sourcemaps: true })
-    .pipe(changed('./dist/js/'))
-    .pipe(plumber(plumberNotify('JS')))
-    .pipe(babel())
-    .pipe(webpack(config))
-    .pipe(dest('./dist/js/', { sourcemaps: true }))
-);
-
-task('images', () =>
-  src('./src/img/**/*')
-    .pipe(changed('./dist/img/'))
-    .pipe(imagemin({ verbose: true }))
-    .pipe(dest('./dist/img/'))
-);
-
-task('fonts', () =>
-  src('./src/fonts/**/*.ttf')
-    .pipe(changed('./dist/fonts/'))
-    .pipe(dest('./dist/fonts/'))
-);
-
-task('files', () =>
-  src('./src/files/**/*')
-    .pipe(changed('./dist/files/'))
-    .pipe(dest('./dist/files/'))
-);
-
-task('server', () =>
-  src('./dist')
-    .pipe(server(serverOptions))
-);
-
-task('clean', async () => await deleteAsync('./dist/'));
-
-task('watch', () => {
-  watch('./src/js/**/*.js', series('js'));
-  watch('./src/sass/**/*.sass', series('sass'));
-  watch('./src/html/**/*.html', series('html'));
-  watch('./src/fonts/**/*', series('fonts'));
-  watch('./src/img/**/*', series('images'));
-  watch('./src/files/**/*', series('files'));
-});
+const { task, series, parallel } = gulp;
 
 task('default', series(
+  'clean:dev',
+  parallel('files:dev', 'images:dev', 'fonts:dev', 'js:dev', 'sass:dev', 'html:dev'),
+  parallel('server:dev', 'watch:dev'),
+));
+
+task('docs', series(
   'clean',
   parallel('files', 'images', 'fonts', 'js', 'sass', 'html'),
-  parallel('server', 'watch'),
+  parallel('server'),
 ));
